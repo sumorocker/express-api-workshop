68f10814ad667621bc7e6a48c42c3b15e6f91c90
sumorocker <macdonald.allan@live.com>
2015-12-01 19:24:12 +0000

    index.js exercise 1

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
 var db = require('mysql');
+
 var connection = db.createConnection({
-    user: 'ziad_saab',
+    user: 'sumorocker',
     host: '127.0.0.1',
     database: 'addressbook'
 });
@@ -11,4 +11,29 @@ var connection = db.createConnection({
 var app = express();
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    req.accountId = 1;
+    next()
+})
+
+app.get('/AddressBook/:accountId', function(req, res) {
+    connection.query('select * from Account where id ='+ req.accountId,
+        function(err, accountInfo) {
+            if (err) {
+                console.log(err)
+            }else if(accountInfo) {
+                console.log(accountInfo)
+            }
+        })
+});
+
+// connection.end();
+
 // app.get('...
+
+var server = app.listen(process.env.PORT, process.env.IP, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
